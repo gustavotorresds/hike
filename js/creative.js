b5d571a4154ecc664233fb01a5d620a8c9829461
Gustavo Torres <gustavotorresds@gmail.com>
2018-07-04 14:54:07 -0300

    Edits according to Joao's design. Missing quotes and hero design

--- js/creative.js
+++ js/creative.js
@@ -30,11 +30,9 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
-      $("#logo-img").attr("src", "img/logo-white.png");
       $("#subscribe-navbar-button").css("border", "1px solid white")
     } else {
       $("#mainNav").removeClass("navbar-shrink");
-      $("#logo-img").attr("src", "img/logo-black.png");
       $("#subscribe-navbar-button").css("border", "1px solid black")
     }
   };
@@ -60,20 +58,5 @@
     distance: '0px'
   }, 300);
 
-  // Magnific popup calls
-  $('.popup-gallery').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
 
 })(jQuery); // End of use strict
